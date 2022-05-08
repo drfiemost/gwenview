ed4bf36203bf40e639d8520d293d4d6d184ec6cb
Leandro Nini <drfiemost@users.noreply.github.com>
2022-05-08 21:36:47 +0200

    Fix automoc handling

--- tests/auto/semanticinfobackendtest.cpp
+++ tests/auto/semanticinfobackendtest.cpp
@@ -18,7 +18,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 */
 // Local
-#include "semanticinfobackendtest.moc"
+#include "moc_semanticinfobackendtest.cpp"
 
 // Qt
 #include <QSignalSpy>
