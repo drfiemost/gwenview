drfiemost/gwenview

commit eef0949019c076d0445650777b9a41ac39848ee6

Fix deprecated warnings

lib/documentview/abstractimageview.cpp
@@ -86,8 +86,8 @@ struct AbstractImageViewPrivate
         QSizeF zoomedDocSize = q->documentSize() * mZoom;
         QSizeF viewSize = q->boundingRect().size();
         QPointF newPos(
-            qBound(qreal(0.), _newPos.x(), zoomedDocSize.width() - viewSize.width()),
-            qBound(qreal(0.), _newPos.y(), zoomedDocSize.height() - viewSize.height())
+            std::clamp(_newPos.x(), qreal(0.), zoomedDocSize.width() - viewSize.width()),
+            std::clamp(_newPos.y(), qreal(0.), zoomedDocSize.height() - viewSize.height())
         );
         if (newPos != mScrollPos) {
             QPointF oldPos = mScrollPos;

lib/crop/croptool.cpp
@@ -68,7 +68,7 @@ Q_DECLARE_FLAGS(CropHandle, CropHandleFlag)
 inline QPoint boundPointX(const QPoint& point, const QRect& rect)
 {
     return QPoint(
-               qBound(rect.left(), point.x(), rect.right()),
+               std::clamp(point.x(), rect.left(), rect.right()),
                point.y()
            );
 }
@@ -76,8 +76,8 @@ inline QPoint boundPointX(const QPoint& point, const QRect& rect)
 inline QPoint boundPointXY(const QPoint& point, const QRect& rect)
 {
     return QPoint(
-               qBound(rect.left(), point.x(), rect.right()),
-               qBound(rect.top(),  point.y(), rect.bottom())
+               std::clamp(point.x(), rect.left(), rect.right()),
+               std::clamp(point.y(), rect.top(), rect.bottom())
            );
 }
 
@@ -112,7 +112,7 @@ struct CropToolPrivate
             top = rect.bottom() - HANDLE_SIZE;
         } else {
             top = rect.top() + (rect.height() - HANDLE_SIZE) / 2;
-            top = qBound(0, top, viewportSize.height() - HANDLE_SIZE);
+            top = std::clamp(top, 0, viewportSize.height() - HANDLE_SIZE);
         }
 
         if (handle & CH_Left) {
@@ -121,7 +121,7 @@ struct CropToolPrivate
             left = rect.right() - HANDLE_SIZE;
         } else {
             left = rect.left() + (rect.width() - HANDLE_SIZE) / 2;
-            left = qBound(0, left, viewportSize.width() - HANDLE_SIZE);
+            left = std::clamp(left, 0, viewportSize.width() - HANDLE_SIZE);
         }
 
         return QRect(left, top, HANDLE_SIZE, HANDLE_SIZE);
@@ -317,8 +317,8 @@ void CropTool::mouseMoveEvent(QGraphicsSceneMouseEvent* event)
     const QSize imageSize = imageView()->document()->size();
 
     QPoint point = imageView()->mapToImage(event->pos().toPoint());
-    int posX = qBound(0, point.x(), imageSize.width() - 1);
-    int posY = qBound(0, point.y(), imageSize.height() - 1);
+    int posX = std::clamp(point.x(), 0, imageSize.width() - 1);
+    int posY = std::clamp(point.y(), 0, imageSize.height() - 1);
 
     if (d->mMovingHandle == CH_None) {
         return;

lib/documentview/documentview.cpp
@@ -240,7 +240,7 @@ struct DocumentViewPrivate
     void setZoom(qreal zoom, const QPointF& center = QPointF(-1, -1))
     {
         uncheckZoomToFit();
-        zoom = qBound(q->minimumZoom(), zoom, MAXIMUM_ZOOM_VALUE);
+        zoom = std::clamp(zoom, q->minimumZoom(), MAXIMUM_ZOOM_VALUE);
         mAdapter->setZoom(zoom, center);
     }
 
@@ -616,7 +616,7 @@ qreal DocumentView::minimumZoom() const
 {
     // There is no point zooming out less than zoomToFit, but make sure it does
     // not get too small either
-    return qBound(qreal(0.001), d->mAdapter->computeZoomToFit(), qreal(1.));
+    return std::clamp(d->mAdapter->computeZoomToFit(), qreal(0.001), qreal(1.));
 }
 
 void DocumentView::setCompareMode(bool compare)

lib/hud/hudslider.cpp
@@ -291,7 +291,7 @@ void HudSlider::setSingleStep(int step)
 
 void HudSlider::setValue(int value)
 {
-    value = qBound(d->mMin, value, d->mMax);
+    value = std::clamp(value, d->mMin, d->mMax);
     if (value != d->mValue) {
         d->mValue = value;
         setSliderPosition(value);
@@ -307,7 +307,7 @@ int HudSlider::sliderPosition() const
 
 void HudSlider::setSliderPosition(int pos)
 {
-    pos = qBound(d->mMin, pos, d->mMax);
+    pos = std::clamp(pos, d->mMin, d->mMax);
     if (pos != d->mSliderPosition) {
         d->mSliderPosition = pos;
         d->updateHandleRect();

lib/paintutils.cpp
@@ -124,9 +124,9 @@ QColor adjustedHsv(const QColor& color, int deltaH, int deltaS, int deltaV)
     int hue, saturation, value;
     color.getHsv(&hue, &saturation, &value);
     return QColor::fromHsv(
-               qBound(0, hue + deltaH, 359),
-               qBound(0, saturation + deltaS, 255),
-               qBound(0, value + deltaV, 255)
+               std::clamp(hue + deltaH, 0, 359),
+               std::clamp(saturation + deltaS, 0, 255),
+               std::clamp(value + deltaV, 0, 255)
            );
 }
 

lib/redeyereduction/redeyereductionimageoperation.cpp
@@ -124,7 +124,7 @@ inline qreal computeRedEyeAlpha(const QColor& src)
         axs = ramp(sat);
     }
 
-    return qBound(qreal(0.), src.alphaF() * axs, qreal(1.));
+    return std::clamp(src.alphaF() * axs, qreal(0.), qreal(1.));
 }
 
 void RedEyeReductionImageOperation::apply(QImage* img, const QRectF& rectF)

commit ba92abe1655219884f73f1470be635ac6c8d110f

Fix regression introduced with eef0949

lib/documentview/abstractimageview.cpp
@@ -86,8 +86,8 @@ struct AbstractImageViewPrivate
         QSizeF zoomedDocSize = q->documentSize() * mZoom;
         QSizeF viewSize = q->boundingRect().size();
         QPointF newPos(
-            std::clamp(_newPos.x(), qreal(0.), zoomedDocSize.width() - viewSize.width()),
-            std::clamp(_newPos.y(), qreal(0.), zoomedDocSize.height() - viewSize.height())
+            std::clamp(_newPos.x(), qreal(0.), std::max(qreal(0.), zoomedDocSize.width() - viewSize.width())),
+            std::clamp(_newPos.y(), qreal(0.), std::max(qreal(0.), zoomedDocSize.height() - viewSize.height()))
         );
         if (newPos != mScrollPos) {
             QPointF oldPos = mScrollPos;